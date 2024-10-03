a7407a9c44453b57f885a8ad53e5086a5b43c486
UTTAM TYAGI <137704401+uttamtyagi111@users.noreply.github.com>
2024-10-04 04:58:11 +0530

    commit csrf allowed host

--- email_automation/settings.py
+++ email_automation/settings.py
@@ -27,7 +27,9 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
 
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-api-aqlo.onrender.com',
+]
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/login/'
