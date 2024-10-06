c2b507df0e8aa7f7e6d6c3f55de810683bb3c945
UTTAM TYAGI <137704401+uttamtyagi111@users.noreply.github.com>
2024-10-06 05:41:27 +0530

    commit cors allowed origins

--- email_automation/settings.py
+++ email_automation/settings.py
@@ -95,6 +95,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://wishgeeksdigital.com',
+    'https://email-automation-mocha.vercel.app/',
     
 ]
 
