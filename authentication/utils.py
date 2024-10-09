aa30763f28776d36073785c21faf1c7d7ea023ad
UTTAM TYAGI <137704401+uttamtyagi111@users.noreply.github.com>
2024-10-09 23:22:02 +0530

    commit added password reset template

--- authentication/utils.py
+++ authentication/utils.py
@@ -1,7 +1,13 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.conf import settings
+from django.contrib.auth.models import User
 import random
 from django.conf import settings
 from django.core.mail import EmailMessage
-from django.template.loader import render_to_string
 
 def generate_otp():
     return str(random.randint(100000, 999999))
@@ -11,7 +17,7 @@ def send_otp_email(email, otp, username):
     """
     Send an OTP email to the specified address with HTML content.
     """
-    subject = 'Your WishgeeksTechserve Registration OTP'
+    subject = 'Your Wish Geeks Techserve Registration OTP'
     from_email = settings.DEFAULT_FROM_EMAIL
 
     html_content = render_to_string('emails/otp_email.html', {'otp': otp, 'username': username})
@@ -25,3 +31,30 @@ def send_otp_email(email, otp, username):
     email_message.content_subtype = "html"  
     email_message.send()
 
+
+def send_password_reset_email(user, base_url):
+    """
+    Sends password reset email to the specified user.
+    
+    :param user: User object (the recipient)
+    :param base_url: The base URL of the site to generate the reset link
+    :return: None
+    """
+    uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+    token = default_token_generator.make_token(user)
+    reset_link = f"{base_url}/reset_password/{uidb64}/{token}/"
+    
+    # Render the HTML email template
+    html_content = render_to_string('emails/password_reset_email.html', {'reset_link': reset_link, 'username': user.username})
+    subject = 'Your Wish Geeks Techserve Password Reset Request'
+    from_email = settings.DEFAULT_FROM_EMAIL
+
+    # Send email with both HTML and plain text versions
+    email_message = EmailMultiAlternatives(
+        subject=subject,
+        body=f"Click the link to reset your password: {reset_link}",  # plain text content
+        from_email=from_email,
+        to=[user.email]
+    )
+    email_message.attach_alternative(html_content, "text/html")
+    email_message.send()
--- authentication/views.py
+++ authentication/views.py
@@ -10,17 +10,19 @@ from .forms import CreateUserForm, EmailLoginForm, PasswordResetRequestForm, Set
 from .forms import OTPVerificationForm
 from django.core.mail import send_mail
 from django.conf import settings
+from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth.models import User
+from .forms import PasswordResetRequestForm
+from .utils import send_password_reset_email  
 import random,logging
 from django.shortcuts import render
 from django.core.cache import cache
 from .utils import generate_otp, send_otp_email 
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import JsonResponse
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate, login as django_login,logout
 from rest_framework.permissions import IsAuthenticated
@@ -217,16 +219,8 @@ def request_password_reset(request):
         user = User.objects.filter(email=email).first()
 
         if user:
-            uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
-            token = default_token_generator.make_token(user)
-            reset_link = f"{BASE_URL}/reset_password/{uidb64}/{token}/"
-            send_mail(
-                'Password Reset Request',
-                f'Click the link to reset your password: {reset_link}',
-                settings.DEFAULT_FROM_EMAIL,
-                [email],
-                fail_silently=False,
-            )
+            # Use the utility function to send the reset email
+            send_password_reset_email(user, settings.BASE_URL)
             return Response({'message': 'Password reset email sent.'}, status=status.HTTP_200_OK)
         else:
             return Response({'errors': {'email': ['No user found with this email address.']}}, status=status.HTTP_400_BAD_REQUEST)
@@ -258,6 +252,7 @@ def reset_password(request, uidb64, token):
         form = SetNewPasswordForm()
 
     return Response({'form': form.as_p()}, status=status.HTTP_200_OK)
+
 
 
 
