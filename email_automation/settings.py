f06ecf8fe041694b15bccaaf8937a6dd845e9de5
UTTAM TYAGI <137704401+uttamtyagi111@users.noreply.github.com>
2024-10-15 22:21:07 +0530

    commit added challes to middleware

--- email_automation/settings.py
+++ email_automation/settings.py
@@ -87,6 +87,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
+    'channels.middleware.WebSocketMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
