8982db3b2b124be17f9c5bcddc077e291e726fef
UTTAM TYAGI <137704401+uttamtyagi111@users.noreply.github.com>
2024-11-13 02:37:41 +0530

    commit signals.py updated

--- subscriptions/signals.py
+++ subscriptions/signals.py
@@ -25,9 +25,9 @@ def create_user_profile(sender, instance, created, **kwargs):
         profile.save()
         # instance.UserProfile.save()
         
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.userprofile.save()
 # from django.db.models.signals import post_save
 # from django.dispatch import receiver
 # from django.contrib.auth.models import User
