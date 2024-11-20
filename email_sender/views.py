465ebcd51002e18342a73e8805eb32e952ef4c93
UTTAM TYAGI <137704401+uttamtyagi111@users.noreply.github.com>
2024-11-21 00:00:20 +0530

    commit deleted db

--- email_sender/views.py
+++ email_sender/views.py
@@ -263,7 +263,7 @@ class SendEmailsView(APIView):
             return Response({"message": message}, status=status.HTTP_400_BAD_REQUEST)
            
         if profile.plan_status == 'expired':
-            return Response({'error': 'Your account is inactive. Please select a plan to continue.'}, status=status.HTTP_403_FORBIDDEN)
+            return Response({'error': 'Your Trial is expired. Please select a plan to continue.'}, status=status.HTTP_403_FORBIDDEN)
         
         email_limit = profile.current_plan.email_limit if profile.current_plan else self.DEFAULT_EMAIL_LIMIT
 
